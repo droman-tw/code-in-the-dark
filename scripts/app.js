c657683483d19bc597cebef3d3d8d9bfb7aefbc2
Roberto Paredes <rdparedessalazar@gmail.com>
2016-02-22 22:52:04 -0500

    Adds copy for competition explanation

--- scripts/app.js
+++ scripts/app.js
@@ -2,7 +2,7 @@
 
     $(function() {
         var playButton = $('.play-button');
-        var videoIframeHtml = '<iframe src="http://player.vimeo.com/video/63318482?title=0&byline=0&portrait=0&autoplay=1" width="700" height="467"></iframe>';
+        var videoIframeHtml = '<iframe src="http://player.vimeo.com/video/156356421?title=0&byline=0&portrait=0&autoplay=1" width="700" height="467"></iframe>';
 
         playButton.on({
 
